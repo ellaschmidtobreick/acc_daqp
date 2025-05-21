e086de4a095370b36f4175c251ea7ef0efd88aa0
ellaschmidtobreick <ella.schmidtobreick.4283@student.uu.se>
2025-05-21 12:14:43 +0200

    fix bug

--- naive_model.py
+++ naive_model.py
@@ -1,5 +1,8 @@
+import numpy as np
+
 # Naive model
-def naive_model(all_labels):
+def naive_model(n_vector,m_vector,all_labels):
+    # Compute node metrics
     preds = [0 for i in range(len(all_labels))]
     tp = 0
     fp = 0
@@ -15,5 +18,36 @@ def naive_model(all_labels):
     prec = tp / (tp + fp) if (tp + fp) > 0 else 0
     rec = tp / (tp + fn) if (tp + fn) > 0 else 0
     f1 = 2 * (prec * rec) / (prec + rec) if (prec + rec) > 0 else 0
+    
+    # Initialize
+    graph_pred = []
+    num_wrongly_pred_nodes_per_graph = []
+    perc_wrongly_pred_nodes_per_graph = []
+    
+    # Compute graph metric
+    current = 0
+    for n, m in zip(n_vector, m_vector):
+        all_labels_graph = np.array(all_labels[current:current+(n+m)])
+        preds_graph = np.array(preds[current:current+(n+m)])     
+        graph_pred.append(preds_graph == all_labels_graph)
+        
+        wrong = np.abs((n+m) - np.sum(all_labels_graph == preds_graph))
+        num_wrongly_pred_nodes_per_graph.append(wrong)
+        perc_wrongly_pred_nodes_per_graph.append([wrong / (n + m)])
 
-    return acc, prec, rec, f1
+        current += (n+m)
+        
+    
+    # for i in range(len(n_vector)):
+    #     n = int(n_vector[i])
+    #     m = int(m_vector[i])
+        
+    #     # Compute grph metrics
+    #     preds_graph = np.asarray(preds).reshape(-1,n+m)
+    #     all_labels_graph = np.asarray(all_labels).reshape(-1,n+m)
+    #     graph_pred.extend(np.all(preds_graph == all_labels_graph, axis=1))
+        
+    #     num_wrongly_pred_nodes_per_graph.extend(np.abs((n+m) - np.sum(all_labels_graph == preds_graph, axis=1)))
+    #     perc_wrongly_pred_nodes_per_graph.extend([(x / (n + m)) for x in num_wrongly_pred_nodes_per_graph])
+
+    return acc, prec, rec, f1, perc_wrongly_pred_nodes_per_graph
