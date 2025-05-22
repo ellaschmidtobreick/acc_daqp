54b1150930844d9e7968da87546407f0fd1b64b4
ellaschmidtobreick <ella.schmidtobreick.4283@student.uu.se>
2025-05-22 03:15:55 +0200

    threshold tuning

---
+++ threshold_tuning.py
@@ -0,0 +1,32 @@
+from train_model import train_GNN
+import numpy as np
+
+# Set parameters
+n = [2]
+m = [5]
+
+nth = 2
+seed = 123
+data_points = 5000
+lr = 0.001
+number_of_max_epochs = 100
+layer_width = 128
+number_of_layers = 3
+track_on_wandb = False
+t = 0.4
+
+threshold = np.arange(0.1,1,0.1)
+#Threshold tuning
+best_threshold = 0
+best_mean = np.inf
+for t in threshold:
+    epoch, train_acc_save, val_acc_save, train_loss_save, val_loss_save, train_prec_save, val_prec_save, train_rec_save, val_rec_save, train_f1_save, val_f1_save, acc_graph_train_save, acc_graph_val_save, val_perc_wrongly_pred_nodes_per_graph_save, val_mean_wrongly_pred_nodes_per_graph_save = train_GNN(n,m,nth, seed, data_points,lr,number_of_max_epochs,layer_width,number_of_layers, track_on_wandb,t, False,False)
+
+    #Threshold tuning
+    if val_mean_wrongly_pred_nodes_per_graph_save[epoch-6] < best_mean:
+        best_threshold = t
+        best_mean = val_mean_wrongly_pred_nodes_per_graph_save[epoch-6]
+    print(t, val_mean_wrongly_pred_nodes_per_graph_save[epoch-6])
+        
+print("Best threshold: t=",best_threshold)
+print("with best mean per wrongly predicted noder per graph:", best_mean)
