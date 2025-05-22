88a6580c96cf8b1cf172ed803a174ecdf4729a65
ellaschmidtobreick <ella.schmidtobreick.4283@student.uu.se>
2025-05-22 03:18:29 +0200

    commented out training

--- experiment_train_25v_100c.py
+++ experiment_train_25v_100c.py
@@ -15,8 +15,7 @@ number_of_layers = 3
 track_on_wandb = True
 t = 0.6
 
-# Train 4 different models to compare the metrics depending on H being fixed or flexible
-# Test the 4 different models on the according test data
-train_GNN(n,m,nth, seed, data_points,lr,number_of_max_epochs,layer_width,number_of_layers, track_on_wandb,t, False,False,"model_25v_100c_fixedHA")
+# Train the model on scaled data
+#train_GNN(n,m,nth, seed, data_points,lr,number_of_max_epochs,layer_width,number_of_layers, track_on_wandb,t, False,False,"model_25v_100c_fixedHA")
 test_GNN(n,m,nth, seed, data_points,layer_width,number_of_layers,t, False,False,"model_25v_100c_fixedHA") 
 
