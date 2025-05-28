eec86c5f30a502ca23fc332cded3500ff4f7b873
ellaschmidtobreick <ella.schmidtobreick.4283@student.uu.se>
2025-05-28 10:15:43 +0200

    moved to archive

--- config.py
+++ config.py
@@ -16,19 +16,11 @@ number_of_max_epochs = 100
 layer_width = 128
 number_of_layers = 3
 track_on_wandb = True
-t = 0.4
+t = 0.6
 
-threshold = np.arange(0.1,1,0.1)
-#Threshold tuning
-best_threshold = 0
-best_mean = np.inf
-for t in threshold:
-    epoch, train_acc_save, val_acc_save, train_loss_save, val_loss_save, train_prec_save, val_prec_save, train_rec_save, val_rec_save, train_f1_save, val_f1_save, acc_graph_train_save, acc_graph_val_save, val_perc_wrongly_pred_nodes_per_graph_save, val_mean_wrongly_pred_nodes_per_graph_save = train_GNN(n,m,nth, seed, data_points,lr,number_of_max_epochs,layer_width,number_of_layers, track_on_wandb,t, False,False)
+epoch_fixedHA, train_acc_fixedHA, val_acc_fixedHA, train_loss_fixedHA, val_loss_fixedHA, train_prec_fixedHA, val_prec_fixedHA, train_rec_fixedHA, val_rec_fixedHA, train_f1_fixedHA, val_f1_fixedHA, acc_graph_train_fixedHA, acc_graph_val_fixedHA, val_perc_wrongly_pred_nodes_per_graph_fixedHA, val_mean_wrongly_pred_nodes_per_graph_fixedHA = train_GNN(n,m,nth, seed, data_points,lr,number_of_max_epochs,layer_width,number_of_layers, track_on_wandb,t, False,False)
+epoch_fixedH, train_acc_fixedH, val_acc_fixedH, train_loss_fixedH, val_loss_fixedH, train_prec_fixedH, val_prec_fixedH, train_rec_fixedH, val_rec_fixedH, train_f1_fixedH, val_f1_fixedH, acc_graph_train_fixedH, acc_graph_val_fixedH, val_perc_wrongly_pred_nodes_per_graph_fixedH, val_mean_wrongly_pred_nodes_per_graph_fixedH = train_GNN(n,m,nth, seed, data_points,lr,number_of_max_epochs,layer_width,number_of_layers, track_on_wandb,t, False,True)
+epoch_fixedA, train_acc_fixedA, val_acc_fixedA, train_loss_fixedA, val_loss_fixedA, train_prec_fixedA, val_prec_fixedA, train_rec_fixedA, val_rec_fixedA, train_f1_fixedA, val_f1_fixedA, acc_graph_train_fixedA, acc_graph_val_fixedA, val_perc_wrongly_pred_nodes_per_graph_fixedA, val_mean_wrongly_pred_nodes_per_graph_fixedA = train_GNN(n,m,nth, seed, data_points,lr,number_of_max_epochs,layer_width,number_of_layers, track_on_wandb,t, True,False)
+epoch_flex, train_acc_flex, val_acc_flex, train_loss_flex, val_loss_flex, train_prec_flex, val_prec_flex, train_rec_flex, val_rec_flex, train_f1_flex, val_f1_flex, acc_graph_train_flex, acc_graph_val_flex, val_perc_wrongly_pred_nodes_per_graph_flex, val_mean_wrongly_pred_nodes_per_graph_flex = train_GNN(n,m,nth, seed, data_points,lr,number_of_max_epochs,layer_width,number_of_layers, track_on_wandb,t, True,True)
 
-    #Threshold tuning
-    if val_mean_wrongly_pred_nodes_per_graph_save[epoch-6] < best_mean:
-        best_threshold = t
-        best_mean = val_mean_wrongly_pred_nodes_per_graph_save[epoch-6]
-        
-print("Best threshold: t=",best_threshold)
-print("with best mean per wrongly predicted noder per graph:", best_mean)
+
