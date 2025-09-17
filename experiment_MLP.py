299624b360d9ed1b0a6a410f8cbc0180ce4fc30f
ellaschmidtobreick <ella.schmidtobreick.4283@student.uu.se>
2025-09-17 10:11:42 +0200

    run MLP experiments

---
+++ experiment_MLP.py
@@ -0,0 +1,31 @@
+from train_model import train_MLP
+from test_model import test_MLP
+
+# Set parameters
+n = [10]
+m = [40]
+nth = 2
+seed = 123
+data_points = 5000
+lr = 0.001
+number_of_max_epochs = 100
+layer_width = 128
+number_of_layers = 3
+track_on_wandb = True
+t = 0.6
+
+# train_MLP(n,m,nth, seed, data_points,lr,number_of_max_epochs,layer_width,number_of_layers, track_on_wandb,t, False,False,"MLP_model_10v_40c_fixedH")
+# test_MLP(n,m,nth, seed, data_points,layer_width,number_of_layers,t, False,False,"MLP_model_10v_40c_fixedH")
+
+# train_MLP(n,m,nth, seed, data_points,lr,number_of_max_epochs,layer_width,number_of_layers, track_on_wandb,t, False,True,"MLP_model_10v_40c_fixedH")
+# test_MLP(n,m,nth, seed, data_points,layer_width,number_of_layers,t, False,True,"MLP_model_10v_40c_fixedH")
+
+# train_MLP(n,m,nth, seed, data_points,lr,number_of_max_epochs,layer_width,number_of_layers, track_on_wandb,t, True,False,"MLP_model_10v_40c_fixedA")
+# test_MLP(n,m,nth, seed, data_points,layer_width,number_of_layers,t, True,False,"MLP_model_10v_40c_fixedA")
+
+# train_MLP(n,m,nth, seed, data_points,lr,number_of_max_epochs,layer_width,number_of_layers, track_on_wandb,t, True,True,"MLP_model_10v_40c_flex")
+# test_MLP(n,m,nth, seed, data_points,layer_width,number_of_layers,t, True,True,"MLP_model_10v_40c_flex")
+
+
+train_MLP([25],[100],nth, seed, data_points,lr,number_of_max_epochs,layer_width,number_of_layers, track_on_wandb,t, False,False,"MLP_model_25v_100c_fixedHA")
+test_MLP([25],[100],nth, seed, data_points,layer_width,number_of_layers,t, False,False,"MLP_model_25v_100c_fixedHA")
