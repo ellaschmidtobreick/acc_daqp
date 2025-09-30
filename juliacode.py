66a99fa739d0af1a4ea18c9b1835f7cb4ff11243
Ella Schmidtobreick <ella-johanna.schmidtobreick@it.uu.se>
2025-09-30 10:35:07 +0200

    julia works in WSL

--- juliacode.py
+++
@@ -1,8 +0,0 @@
-import julia
-julia.install(julia="C:\\Users\\ellsc479\\acc_daqp\\acc_daqp_venv\\julia_env\\pyjuliapkg\\install\\bin\\julia.exe")
-
-# from julia import Base
-# print(Base.VERSION)
-
-# import platform
-# print(platform.architecture())
