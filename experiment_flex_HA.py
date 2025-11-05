28d7d0fa0abf58cc0e69ced13034b4aa54fb1f14
Ella Schmidtobreick <ella-johanna.schmidtobreick@it.uu.se>
2025-11-05 22:34:11 +0100

    experiment flexible H/A

---
+++ experiment_flex_HA.py
@@ -0,0 +1,29 @@
+from train_model import train_GNN
+from test_model import test_GNN
+
+# Set parameters
+n = [10]
+m = [40]
+nth = 2
+seed = 123
+data_points = 2000
+lr = 0.001
+number_of_max_epochs = 100
+layer_width = 128
+number_of_layers = 3
+track_on_wandb = False 
+t = 0.6
+
+# Train 4 different models to compare the metrics depending on H being fixed or flexible
+# Test the 4 different models on the according test data
+train_GNN(n,m,nth, seed, data_points,lr,number_of_max_epochs,layer_width,number_of_layers, track_on_wandb,t, False,False,"model_10v_40c_fixedHA",dataset_type="standard")
+test_GNN(n,m,nth, seed, data_points,layer_width,number_of_layers,t, False,False,"model_10v_40c_fixedHA",dataset_type="standard") 
+
+train_GNN(n,m,nth, seed, data_points,lr,number_of_max_epochs,layer_width,number_of_layers, track_on_wandb,t, False,True,"model_10v_40c_fixedH",dataset_type="standard")
+test_GNN(n,m,nth, seed, data_points,layer_width,number_of_layers,t, False,True,"model_10v_40c_fixedH",dataset_type="standard")
+
+train_GNN(n,m,nth, seed, data_points,lr,number_of_max_epochs,layer_width,number_of_layers, track_on_wandb,t, True,False,"model_10v_40c_fixedA",dataset_type="standard")
+test_GNN(n,m,nth, seed, data_points,layer_width,number_of_layers,t, True,False,"model_10v_40c_fixedA",dataset_type="standard")
+
+train_GNN(n,m,nth, seed, data_points,lr,number_of_max_epochs,layer_width,number_of_layers, track_on_wandb,t, True,True,"model_10v_40c_flex",dataset_type="standard")
+test_GNN(n,m,nth, seed, data_points,layer_width,number_of_layers,t, True,True,"model_10v_40c_flex",dataset_type="standard")
